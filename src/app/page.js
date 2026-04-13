a62d3f5c0dc9364906da0d6b886d6f63b9fb0fe1
Ida <idla0004@stud.kea.dk>
2026-04-13 09:32:44 +0200

    Search bar made

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
 import Image from "next/image";
+import { LiaSearchSolid } from "react-icons/lia";
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
-        <section className=""></section>
+        <section className="">
+          <div className="flex justify-between p-4">
+            <LiaSearchSolid
+              color="white"
+              size={45}
+              className=" bg-[#F2968F] items-start p-2 rounded-full"
+            />
+            <input
+              type="search"
+              className="border text-[#CACACD] rounded-full px-3 py-2"
+              placeholder="Search breeds"
+            ></input>
+          </div>
+        </section>
       </main>
     </div>
   );
