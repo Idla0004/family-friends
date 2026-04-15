4e601e3dad4003a5d3ce1f5553ab81858bb90fed
Ida <idla0004@stud.kea.dk>
2026-04-15 08:45:23 +0200

    Opdateret styling på index

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <section className="grid grid-cols-[repeat(auto-fill.minmax(250px,1fr))] gap-4">
       <SearchBar />
-      <section className="flex flex-wrap justify-center gap-7">
+      <section className="flex flex-wrap justify-center gap-x-4 mb-3">
         <BreedList />
       </section>
     </section>
--- src/components/DogBreed.jsx
+++ src/components/DogBreed.jsx
@@ -7,10 +7,10 @@ const DogBreed = ({
   imgsrc,
 }) => {
   return (
-    <div className="bg-white shadow-sm rounded-2xl">
+    <div className="bg-white shadow-sm rounded-2xl mb-3 w-fit">
       <div className="relative">
         <CiStar
-          color="grey"
+          color="white"
           size={24}
           className=" bg-[#fefefe3f] items-center m-2 p-0.5 rounded-full absolute right-0"
         />
@@ -19,14 +19,14 @@ const DogBreed = ({
           alt="beautiful dog"
           width={160}
           height={114}
-          className="rounded-2xl"
+          className="rounded-2xl object-cover aspect-3/2"
         />
       </div>
-      <div className="m-3 flex">
+      <div className="m-3">
         <h3 className="text-[#333333] text-base font-bold">
           {breedGroup}
         </h3>
-        <p className="text-[#333333] opacity-40 text-xs">
+        <p className="text-[#333333] opacity-40 text-xs w-30">
           {origin}
         </p>
       </div>
