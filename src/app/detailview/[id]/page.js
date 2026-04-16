8054c0e7ed7a27c31a688e164190cbc0dcc2cd0c
Ida <idla0004@stud.kea.dk>
2026-04-16 18:00:00 +0200

    opdateret cache fordi vercel ikke ville hoste

--- src/app/detailview/[id]/page.js
+++ src/app/detailview/[id]/page.js
@@ -3,8 +3,17 @@ import { IoIosArrowBack } from "react-icons/io";
 import Link from "next/link";
 import FavoriteElement from "@/components/FavoriteElement";
 import Temperement from "@/components/Temperement";
+import { Suspense } from "react";
 
-const DetailView = async ({ params }) => {
+export default function detailview({ params }) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DetailView params={params} />
+    </Suspense>
+  );
+}
+
+async function DetailView({ params }) {
   const { id } = await params;
 
   const response = await fetch(
@@ -16,71 +25,74 @@ const DetailView = async ({ params }) => {
       cache: "no-store",
     },
   );
+
   const breed = await response.json();
 
   return (
     <div className="bg-white mb-3">
-      <div className="relative flex">
-        <Link href={`/`}>
-          <IoIosArrowBack
-            size={24}
-            className=" bg-[#F8F8F8] items-center m-2 p-0.5 rounded-full absolute left-0 text-[#242424]"
+      <Suspense
+        fallback={<div>Loading dog..</div>}
+      >
+        <div className="relative flex">
+          <Link href={`/`}>
+            <IoIosArrowBack
+              size={24}
+              className=" bg-[#F8F8F8] items-center m-2 p-0.5 rounded-full absolute left-0 text-[#242424]"
+            />
+          </Link>
+          <FavoriteElement
+            id={breed.id}
+            breed={breed.name}
           />
-        </Link>
-        <FavoriteElement
-          id={breed.id}
-          breed={breed.name}
-        />
 
-        <div className="absolute left-0 bottom-0 bg-[rgba(255,255,255,0.2)] flex p-3 rounded-xl backdrop-blur-xl m-3">
+          <div className="absolute left-0 bottom-0 bg-[rgba(255,255,255,0.2)] flex p-3 rounded-xl backdrop-blur-xl m-3">
+            <Image
+              src={breed.image.url}
+              alt="beautiful dog"
+              width={32}
+              height={32}
+              className="rounded-lg aspect-square"
+            />
+            <p className="text-[#FFFF] leading-none text-xs ml-2">
+              {breed.name}
+            </p>
+          </div>
+
           <Image
             src={breed.image.url}
             alt="beautiful dog"
-            width={32}
-            height={32}
-            className="rounded-lg aspect-square"
+            width={339}
+            height={360}
+            className="rounded-2xl w-screen aspect-square object-cover h-auto"
           />
-          <p className="text-[#FFFF] leading-none text-xs ml-2">
-            {breed.name}
-          </p>
         </div>
-
-        <Image
-          src={breed.image.url}
-          alt="beautiful dog"
-          width={339}
-          height={360}
-          className="rounded-2xl w-screen aspect-square object-cover h-auto"
-        />
-      </div>
-      <div className="my-3">
-        <h3 className="text-[#333333] text-3xl font-bold">
-          {breed.breed_group}
-        </h3>
-      </div>
-      <div className="flex">
-        <Temperement
-          tempStr={breed.temperament}
-        ></Temperement>
-      </div>
-      <dl className="text-[#333333] my-5">
-        <dt className="text-xs opacity-50 font-medium">
-          Breed Description
-        </dt>
-        <dd className="text-base">
-          {breed.description}
-        </dd>
-      </dl>
-      <dl className="text-[#333333]">
-        <dt className="text-xs opacity-50 font-medium">
-          Breed History
-        </dt>
-        <dd className="text-base">
-          {breed.history}
-        </dd>
-      </dl>
+        <div className="my-3">
+          <h3 className="text-[#333333] text-3xl font-bold">
+            {breed.breed_group}
+          </h3>
+        </div>
+        <div className="flex">
+          <Temperement
+            tempStr={breed.temperament}
+          ></Temperement>
+        </div>
+        <dl className="text-[#333333] my-5">
+          <dt className="text-xs opacity-50 font-medium">
+            Breed Description
+          </dt>
+          <dd className="text-base">
+            {breed.description}
+          </dd>
+        </dl>
+        <dl className="text-[#333333]">
+          <dt className="text-xs opacity-50 font-medium">
+            Breed History
+          </dt>
+          <dd className="text-base">
+            {breed.history}
+          </dd>
+        </dl>
+      </Suspense>
     </div>
   );
-};
-
-export default DetailView;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,15 @@ export default function Home({ searchParams }) {
   return (
     <section className="grid grid-cols-[repeat(auto-fill.minmax(250px,1fr))] gap-4 justify-center">
       <SearchBar />
-      <section className="flex flex-wrap justify-center mb-3">
-        <Suspense
-          fallback={<div>Loading dogs..</div>}
-        >
+      <Suspense
+        fallback={<div>Loading dogs..</div>}
+      >
+        <section className="flex flex-wrap justify-center mb-3">
           <BreedList
             searchParams={searchParams}
           />
-        </Suspense>
-      </section>
+        </section>
+      </Suspense>
     </section>
   );
 }
